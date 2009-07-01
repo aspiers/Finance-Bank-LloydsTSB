bd2eb26cf51c16dff3007ac62c1d4497f22af7f9
Adam Spiers <cpan@adamspiers.org>
2009-07-01 01:17:48 +0100

    add utils.pm which was missing from MANIFEST all along

---
+++ lib/Finance/Bank/LloydsTSB/utils.pm
@@ -0,0 +1,30 @@
+package Finance::Bank::LloydsTSB::utils;
+
+=head1 NAME
+
+Finance::Bank::LloydsTSB::utils - internal utility routines
+
+=cut
+
+use strict;
+use warnings;
+
+use base 'Exporter';
+our @EXPORT_OK = qw(trim debug);
+
+sub trim {
+    my $class = shift;
+    my ($text) = @_;
+    $text =~ s/^\s+//;
+    $text =~ s/\s+$//;
+    return $text;
+}
+
+sub debug {
+    my $class = shift;
+    my $varname = (ref($class) || $class) . "::DEBUG";
+    no strict 'refs';
+    warn @_ if $$varname;
+}
+
+1;
